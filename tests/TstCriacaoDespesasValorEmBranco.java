cdb6c2393e59caa21364848d3d78be19dd382af7
Kairon Velozo <kairon.vzb@gmail.com>
2018-09-05 17:41:34 -0300

    Update TstCriacaoDespesasValorEmBranco.java

--- tests/TstCriacaoDespesasValorEmBranco.java
+++ tests/TstCriacaoDespesasValorEmBranco.java
@@ -6,10 +6,10 @@ import org.junit.Test;
 import app.ContaCorrente;
 import exceptions.ValorEmBrancoException;
 
-class TstCriacaoDespesasValorEmBranco {
+public class TstCriacaoDespesasValorEmBranco {
 	
 	@Test(expected = ValorEmBrancoException.class)
-	void testCriacaoContas() throws ValorEmBrancoException {
+	public void TstCriacaoDespesasValorEmBranco() {
 		int agencia = 1;
 		int conta = 1;
 		int saldo = 0;
