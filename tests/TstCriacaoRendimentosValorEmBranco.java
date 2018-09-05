8319890271919feddb1b7ccc8f147064a61ea4d9
Kairon Velozo <kairon.vzb@gmail.com>
2018-09-05 17:34:24 -0300

    Create TstCriacaoRendimentosValorEmBranco.java

---
+++ tests/TstCriacaoRendimentosValorEmBranco.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import app.ContaCorrente;
+import exceptions.ValorEmBrancoException;
+
+class TstCriacaoRendimentosValorEmBranco {
+	
+	private ContaCorrente contaCorrente;
+	
+	public TstCriacaoRendimentosValorEmBranco() {
+		int agencia = 1;
+		int conta = 1;
+		int saldo = 0;
+		this.contaCorrente = ContaCorrente.obterContaCorrente(agencia, conta, saldo);
+	}
+
+	@Test(expected = ValorEmBrancoException.class)
+	void testCriacaoContas() {
+		this.contaCorrente.criarReceita(0);
+	}
+
+}
